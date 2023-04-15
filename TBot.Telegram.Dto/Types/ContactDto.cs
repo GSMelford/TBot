5ac0a6e3bb4459d4cbea1dfa58437af94980d05d
Maksym Fedorenko <fedorenko.mx@gmail.com>
2023-04-15 21:53:48 +0300

    Added ContactDto

--- TBot.Telegram.Dto/Types/ContactDto.cs
+++ TBot.Telegram.Dto/Types/ContactDto.cs
@@ -1,6 +1,21 @@
+using System.Text.Json.Serialization;
+
 namespace TBot.Telegram.Dto.Types;
 
 public class ContactDto
 {
+    [JsonPropertyName("phone_number")] 
+    public string PhoneNumber { get; set; } = null!;
     
+    [JsonPropertyName("first_name")]
+    public string FirstName { get; set; } = null!;
+    
+    [JsonPropertyName("last_name")]
+    public string LastName { get; set; } = null!;
+    
+    [JsonPropertyName("user_id")]
+    public int UserId { get; set; }
+    
+    [JsonPropertyName("vcard")]
+    public string? Vcard { get; set; }
 }
